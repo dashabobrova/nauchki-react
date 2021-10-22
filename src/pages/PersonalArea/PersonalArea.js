851e3275f18a78123a472a4798a5a4a4737f8093
dashabobrova <work.dbobrova.dasha@gmail.com>
2021-10-22 16:10:56 +0300

    before reject

--- src/pages/PersonalArea/PersonalArea.js
+++ src/pages/PersonalArea/PersonalArea.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import s from './PersonalArea.module.scss';
-
 export const PersonalArea = () => {
     const user = useSelector(state => state.user.userData);
     console.log(user);
